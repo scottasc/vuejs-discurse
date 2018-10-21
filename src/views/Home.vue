3d1aec118da831f5571970f5eb34bff66d869ed7
scottasc <scottschulfer@gmail.com>
2018-10-21 16:44:32 -0500

    creating frontend, building thought and practice components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="home" :style="{ backgroundColor: `rgb(${thought.red}, ${thought.green}, ${thought.blue})` }"> 
+
+    <form v-on:submit.prevent="submit()">
+      <label>Enter a thought:</label>
+      <input type="text" class="form-control" v-model="content">
+      <input type="submit" class="btn btn-primary" value="Submit">
+    </form>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+.home {
+  transition: 7s;
+  width: 100%;
+  height: 100%;
+  margin: 0px;
 }
+
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      content: "",
+      thought: {}
+    };
+  },
+  created: function() {},
+  methods: {
+    submit: function() {
+      var params = {content: this.content};
+      axios
+        .post("http://localhost:3000/api/thoughts", params)
+        .then(response => {
+          this.thought = response.data;
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
+        });
+        this.content = "";
+    }
+  }
+};
 </script>
---
+++ src/views/PracticesCreate.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="practices-new">
+    <h1>{{practice}}</h1>
+  
+  <div>
+  <button v-on:click="createPractice()">Create a practice</button>
+  </div>
+
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      practice: {}
+    };
+  },
+  created: function() {},
+  methods: {
+    createPractice: function() {
+      axios
+        .post("http://localhost:3000/api/practices")
+        .then(response => {
+          this.practice = response.data;
+        });
+      }
+    }
+  }
+</script>
